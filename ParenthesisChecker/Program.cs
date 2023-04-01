fa68d8839c94e6ab1956adbb8785521e7bcc51c6
SOBREMESADELOCO\Usuario <antomalma@gmail.com>
2023-04-01 19:07:46 +0200

    no message

---
+++ ParenthesisChecker/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ParenthesisChecker
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write a text with parenthesis");
+            string response = Console.ReadLine();
+            int open = CountChars(response, '(');
+            int close = CountChars(response, ')');
+            if(close == -1 || open == -1|| open > close)
+            {
+                Console.WriteLine("The parenthesis don't close");
+            }
+            else
+            {
+                Console.WriteLine("The parenthesis close");
+            }
+        }
+        public static int CountChars(string text, char search)
+        {
+            int amount = 0;
+            bool found = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if(text[i] != search && !found)
+                    amount++;
+                else
+                {
+                    found = true;
+                }
+            }
+            if (!found)
+            {
+                amount = -1;
+            }
+            return amount;
+        }
+    }
+}
---
+++ Primes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Primes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string response;
+            Console.WriteLine("Enter a number: ");
+            response = Console.ReadLine();
+            num = int.Parse(response);
+            if (IsPrime(num))
+            {
+                Console.WriteLine(num + ": is prime");
+            }
+            else
+            {
+                Console.WriteLine(num + ": is not prime");
+            }
+        }
+        public static bool IsPrime(int number)
+        {
+            bool result = false;
+            if(number <= 1)
+                return result;
+            if (number == 2)
+                return !result;
+            if (number % 2 == 0)
+                return result;
+            for(int i = number - 1; i > 1; i--)
+            {
+                if(number % i == 0)
+                    return false;
+                else result= true;
+            }
+            return result;
+        }
+    }
+}
